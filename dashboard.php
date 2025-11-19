5c35c67af96df3797f60e342f5fb3fb3f26661ba
WolfStormPT <151835516+WolfStormPT@users.noreply.github.com>
2025-11-19 15:49:56 +0000

    teste2

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,9 @@
 <?php
-// CONEXÃO COM BASE DE DADOS olá
+// CONEXÃO COM BASE DE DADOS
 $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'pap';
-
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
