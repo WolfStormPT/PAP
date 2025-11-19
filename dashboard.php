7d08fb484e2bec8233996b3fd6bd3dee664f2c48
WolfStormPT <151835516+WolfStormPT@users.noreply.github.com>
2025-11-19 14:45:57 +0000

    alteracao de teste

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,10 @@
 <?php
-// CONEXÃO COM BASE DE DADOS
+// CONEXÃO COM BASE DE DADOS olá
 $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'pap';
+
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
